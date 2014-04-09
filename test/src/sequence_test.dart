6c4d68fb78627450c1c2c864d4d8728f20193693
Rasmus Rønn Nielsen <rasmusrnielsen@gmail.com>
2014-04-09 14:08:38 +0200

    Added a few more tests for sequence.

--- test/src/sequence_test.dart
+++ test/src/sequence_test.dart
@@ -12,6 +12,20 @@ Behavior createDummyBehavior(Status status) {
 }
 
 void main() {
+  test("success", () {
+    var behavior1 = createDummyBehavior(Status.SUCCESS);
+    var behavior2 = createDummyBehavior(Status.SUCCESS);
+    var behavior3 = createDummyBehavior(Status.SUCCESS);
+    
+    var sequence = new Sequence([behavior1, behavior2, behavior3]);
+    var status = sequence.update();
+    
+    expect(status, equals(Status.SUCCESS));
+    expect(behavior1.updateCalls, equals(1));
+    expect(behavior2.updateCalls, equals(1));
+    expect(behavior3.updateCalls, equals(1));
+  });
+  
   test("running", () {
     var behavior1 = createDummyBehavior(Status.SUCCESS);
     var behavior2 = createDummyBehavior(Status.SUCCESS);
@@ -22,6 +36,24 @@ void main() {
     var status = sequence.update();
     
     expect(status, equals(Status.RUNNING));
+    expect(sequence.currentChildIndex, 2);
+    
+    expect(behavior1.updateCalls, equals(1));
+    expect(behavior2.updateCalls, equals(1));
+    expect(behavior3.updateCalls, equals(1));
+    expect(behavior4.updateCalls, equals(0));
+  });
+  
+  test("failure", () {
+    var behavior1 = createDummyBehavior(Status.SUCCESS);
+    var behavior2 = createDummyBehavior(Status.SUCCESS);
+    var behavior3 = createDummyBehavior(Status.FAILURE);
+    var behavior4 = createDummyBehavior(Status.SUCCESS);
+    
+    var sequence = new Sequence([behavior1, behavior2, behavior3, behavior4]);
+    var status = sequence.update();
+    
+    expect(status, equals(Status.FAILURE));
     expect(sequence.currentChildIndex, 2);
     
     expect(behavior1.updateCalls, equals(1));
