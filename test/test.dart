4835f5613cb781b1e7e51d6452767577e0fd5876
Rasmus Rønn Nielsen <rasmusrnielsen@gmail.com>
2014-04-09 18:44:28 +0200

    Added selector test to main test file.

--- test/test.dart
+++ test/test.dart
@@ -2,8 +2,10 @@ import 'package:unittest/unittest.dart';
 
 import 'src/behavior_test.dart' as BehaviorTest;
 import 'src/sequence_test.dart' as SequenceTest;
+import 'src/selector_test.dart' as SelectorTest;
 
 main() {
   group('Behavior test', BehaviorTest.main);
   group('Sequence test', SequenceTest.main);
+  group('Selector test', SelectorTest.main);
 }
